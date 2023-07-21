aab37f0cae142579c55e771ff16a50f972627d98
Ay-slim <ayooluwaadedipe@gmail.com>
2023-07-21 09:13:01 -0700

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - A function to implement the bubble sort algorithm
+ * @array: Array to sort
+ * @size: Size of array
+ * Returns: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int all_good = 0;
+	unsigned int i;
+	int t;
+	unsigned int j = size;
+
+	while (!all_good)
+	{
+		all_good = 1;
+		for (i = 0; i < j - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				t = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = t;
+				print_array(array, size);
+				all_good = 0;
+			}
+		}
+		j--;
+	}
+}
