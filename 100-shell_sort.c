4267441c9a66df7b7657df3acdaf18e4a3319d1b
Ay-slim <ayooluwaadedipe@gmail.com>
2023-07-25 14:36:33 -0700

    Shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Implement shell sort with the Knuth sequence
+ * @array: Array to sort
+ * @size: Size of array
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	int interval = 1;
+	int size_casted = (int)size;
+	int i;
+	int temp;
+	int j;
+
+	while (interval < size_casted / 3)
+		interval = interval * 3 + 1;
+	while (interval > 0)
+	{
+		for (i = interval; i < size_casted; i++)
+		{
+			temp = array[i];
+			j = i;
+			while (j >= interval && array[j - interval] > temp)
+			{
+				array[j] = array[j - interval];
+				j -= interval;
+			}
+			array[j] = temp;
+		}
+		interval = (interval - 1) / 3;
+	}
+}
